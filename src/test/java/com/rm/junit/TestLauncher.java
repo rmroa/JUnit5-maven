a124a72d6a4b00d671b6d059d5ac2889832b2ce3
rm <romtho@mail.ru>
2021-11-23 23:48:39 +0300

    Launcher API

---
+++ src/test/java/com/rm/junit/TestLauncher.java
@@ -0,0 +1,34 @@
+package com.rm.junit;
+
+import org.junit.platform.engine.discovery.DiscoverySelectors;
+import org.junit.platform.launcher.Launcher;
+import org.junit.platform.launcher.LauncherDiscoveryRequest;
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
+import org.junit.platform.launcher.core.LauncherFactory;
+import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
+
+import java.io.PrintWriter;
+
+public class TestLauncher {
+
+    public static void main(String[] args) {
+        Launcher launcher = LauncherFactory.create();
+//        launcher.registerLauncherDiscoveryListeners();
+
+        SummaryGeneratingListener summaryGeneratingListener = new SummaryGeneratingListener();
+//        launcher.registerTestExecutionListeners();
+//        launcher.registerTestExecutionListeners(summaryGeneratingListener);
+
+        LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder
+                .request()
+//                .selectors(DiscoverySelectors.selectClass(UserServiceTest.class))
+                .selectors(DiscoverySelectors.selectPackage("com.rm.junit.service"))
+//                .listeners()
+                .build();
+        launcher.execute(request, summaryGeneratingListener);
+
+        try (PrintWriter printWriter = new PrintWriter(System.out)) {
+            summaryGeneratingListener.getSummary().printTo(printWriter);
+        }
+    }
+}
