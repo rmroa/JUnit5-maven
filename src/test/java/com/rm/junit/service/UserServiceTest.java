cc16b81a45a908503d66c9a833b2fe7866793cf8
rm <romtho@mail.ru>
2021-11-24 21:05:50 +0300

    testing exceptions

--- src/test/java/com/rm/junit/service/UserServiceTest.java
+++ src/test/java/com/rm/junit/service/UserServiceTest.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
@@ -93,6 +94,14 @@ class UserServiceTest {
         Optional<User> maybeUser = userService.login("not correct", "123");
 
         assertTrue(maybeUser.isEmpty());
+    }
+
+    @Test
+    void throwExceptionIfUsernameOrPasswordIsNull() {
+        assertAll(
+                () -> assertThrows(IllegalArgumentException.class, () -> userService.login(null, "not correct")),
+                () -> assertThrows(IllegalArgumentException.class, () -> userService.login("not correct", null))
+        );
     }
 
     @AfterEach
